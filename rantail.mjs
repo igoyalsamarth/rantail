3a81ac7e218026dac8542a7e6f0837d907361bb1
Samarth Goyal <samarth.goyal1999@gmail.com>
2024-02-09 20:43:23 +0530

    mjs

--- rantail.mjs
+++ rantail.mjs
@@ -1,6 +1,5 @@
-import fs from 'fs'
-import path from 'path'
-
+const fs = require('fs');
+const path = require('path');
 // Define the mapping from original class names to Tailwind CSS classes
 const tailwindClasses = {
   'text-lg': 'text-lg',
