3a5abb0eecc263107869b42da4d1fd5199f67fad
Samarth Goyal <samarth.goyal1999@gmail.com>
2024-02-09 21:38:13 +0530

    more variables

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 function App() {
   return (
     <div>
-      <p className="text-3xl">test2</p>
+      <p className="text-3xl font-black">test2</p>
       <div className='text-sm'>test-3</div>
     </div>
   )
