7e61d00903284a4a231385c3b87cfec17cd0ea37
Samarth Goyal <samarth.goyal1999@gmail.com>
2024-02-10 18:06:58 +0530

    conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-
+import { useState } from "react";
 function App() {
+  const [number, setNumber] = useState(0)
   return (
     <div>
       <p className="text-3xl font-black">test2</p>
       <div className={`text-sm font-black`}>test-3</div>
+      <button onClick={()=> setNumber(number+1)} className={` ${number%2 === 0 ? 'bg-orange-500' : 'bg-blue-500'} `}>Press Me</button>
     </div>
   )
 }
