f516211d5bcbe2a4e4dea6433230b9eed1bfa6fc
Samarth Goyal <samarth.goyal1999@gmail.com>
2024-02-10 18:49:09 +0530

    dynamic classNames

--- rantail.cjs
+++ rantail.cjs
@@ -21,7 +21,7 @@ let cssContent = '@tailwind base;\n@tailwind components;\n@tailwind utilities;\n
 fs.writeFileSync(cssFilePath, cssContent);
 
 // Use a regular expression to match all class names in the JSX file
-const classNameRegex = /className=(['"])(.*?)\1|className={(`[^`]*?)}/g;
+const classNameRegex = /className=(['"])(.*?)\1|className={`([^`]*?)`}/g;
 let match;
 const tailwindClasses = {};
 
@@ -54,7 +54,7 @@ for (const pattern of config.content) {
       }
 
       // Replace the class name in the JSX file
-      jsxFileContent = jsxFileContent.replace(new RegExp(`(className=(['"])${match[2]}\\2|className={${match[3]}})`, 'g'), (match, p1, p2, p3) => {
+      jsxFileContent = jsxFileContent.replace(new RegExp(`(className=(['"])${match[2]}\\2|className={\`${match[3]}\`})`, 'g'), (match, p1, p2, p3) => {
         if (p2) {
           // If the match is a class name enclosed in quotes
           return `className=${p2}${newClassNames.trim()}${p2}`;
@@ -63,9 +63,9 @@ for (const pattern of config.content) {
           return `className={\`${newClassNames.trim()}\`}`;
         }
       });
-    }
 
-    // Write the modified JSX file
-    fs.writeFileSync(file, jsxFileContent);
+      // Write the modified JSX file
+      fs.writeFileSync(file, jsxFileContent);
+    }
   }
 }
