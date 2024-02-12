6fb19e243b4fd276b7197aa5f68987c1ec1472b3
Samarth Goyal <samarth.goyal1999@gmail.com>
2024-02-12 18:10:39 +0530

    files

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ function App() {
       <p className="text-3xl font-black">test2</p>
       <div className={`text-sm font-black`}>test-3</div>
       <button onClick={()=> setNumber(number+1)} className={`border-[5px] border-yellow-500 rounded-lg ${number%2 === 0 ? 'bg-orange-500 font-bold' : 'bg-blue-500 font-black'} `}>Press Me</button>
-      <TestComponent />
     </div>
   )
 }
