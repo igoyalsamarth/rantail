324d5c912db04045cb8bbc7ed88d4669d76257c9
Samarth Goyal <samarth.goyal1999@gmail.com>
2024-02-12 18:06:55 +0530

    list of files

---
+++ src/components/testComponent.jsx
@@ -0,0 +1,7 @@
+export default function TestComponent(){
+    return(
+        <div className="flex w-[500px] bg-orange-500">
+            <button className="bg-blue-500">Test Button</button>
+        </div>
+    );
+}
