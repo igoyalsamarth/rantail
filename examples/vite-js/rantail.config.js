0175c1066fbd36a87fa2fb390a04da2ff5fea099
Samarth Goyal <samarth.goyal1999@gmail.com>
2024-03-06 22:47:44 +0530

    readme update

--- examples/vite-js/rantail.config.js
+++ examples/vite-js/rantail.config.js
@@ -1,9 +1,11 @@
+/** @type {import('rantail').IConfig} */
+
+
 const config = {
     suffix: '_',
     prefix: '_',
     cuidLength: 5,
-    cssFilePath:'src/index.css',
-    ignore:'_'
+    cssFilePath:'src/index.css'
 }
 
 export default config
