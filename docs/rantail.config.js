028477e3952bdbe0e243b34d58fbc5a2f4f03fcf
Samarth Goyal <samarth.goyal1999@gmail.com>
2024-03-08 15:42:24 +0530

    attempt 2 at v0.0.2

--- docs/rantail.config.js
+++ docs/rantail.config.js
@@ -6,4 +6,10 @@ module.exports ={
     prefix: '_',
     cuidLength: 5,
     cssFilePath:'app/global.css',
+    content: [
+        './pages/**/*.{js,jsx,ts,tsx}',
+        './components/**/*.{js,jsx,ts,tsx}',
+        './app/**/*.{js,jsx,ts,tsx}',
+        './src/**/*.{js,jsx,ts,tsx}',
+    ]
 }
