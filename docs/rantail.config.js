ff2926d719e7932001da2da4c60ae032600ccba2
Samarth Goyal <samarth.goyal1999@gmail.com>
2024-03-08 15:34:45 +0530

    v0.0.2

---
+++ docs/rantail.config.js
@@ -0,0 +1,11 @@
+/** @type {import('rantail').IConfig} */
+
+
+const config = {
+    suffix: '_',
+    prefix: '_',
+    cuidLength: 5,
+    cssFilePath:'app/global.css',
+}
+
+export default config
